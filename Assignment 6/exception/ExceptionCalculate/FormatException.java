b084d14421228155e12e8682c28118459fae9ce6
Jaswant1002 <68215756+Jaswant1002@users.noreply.github.com>
2020-07-25 01:48:44 +0530

    Add files via upload

---
+++ Assignment 6/exception/ExceptionCalculate/FormatException.java
@@ -0,0 +1,10 @@
+package ExceptionCalculate;
+
+public class FormatException extends Exception {
+
+	public FormatException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
